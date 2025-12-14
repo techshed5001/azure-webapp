089b3327265f80327718b74c9681c08f14009377
techshed5001 <charlie.long@yahoo.com>
2025-12-14 17:43:14 -0600

    updated azurerm version

--- providers.tf
+++ providers.tf
@@ -1,13 +1,12 @@
 /*
-This Terraform code block defines the required provider and its version for the Azure Resource Manager (azurerm) provider. 
-It also configures the provider to use default features.
+
 */
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.68.0"
+      version = "4.56.0"
     }
   }
 }
