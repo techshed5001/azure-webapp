9632cf5360d247b4e357e900751324e585e441ff
techshed5001 <charlie.long@yahoo.com>
2025-12-14 20:08:25 -0600

    corrected spelling error

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "azure-webapp-rg"
-    storage_account_name = "azurewebappsa"
+    storage_account_name = "azurewebapsa"
     container_name       = "azurewebappco"
     key                  = "./terraform.tfstate"
   }
