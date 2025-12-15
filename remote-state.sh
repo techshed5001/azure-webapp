f60fb0718a722e01cf4204b4b5d0ac5f12d377e0
techshed5001 <charlie.long@yahoo.com>
2025-12-14 19:46:05 -0600

    corrected state file name

---
+++ remote-state.sh
@@ -0,0 +1,17 @@
+# Login to Azure (if not already logged in)
+    # Login to Azure (if not already logged in)
+az account show 1> /dev/null || az login
+
+rg=azure-webapp-rg
+sa=azurewebapsa
+container=azurewebappco
+
+# Resource group
+az group create --name $rg --location eastus --tags 'Project=Terraform' 'Env=Demo'
+
+# account
+az storage account create --resource-group $rg --name $sa --sku Standard_LRS --encryption-services blob
+
+# container
+az storage container create --name $container --account-name $sa
+  
